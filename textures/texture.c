cf422cdcb4fabb8e99cda50423fd955d9c84f1c5
markus <marmomag@gmail.com>
2025-11-15 02:23:33 +0100

    forgot to save some files

--- textures/texture.c
+++ textures/texture.c
@@ -1,6 +1,2 @@
 #include "texture.h"
 
-texture solid_color_albedo(color albedo);
-texture solid_color_rgb(double red, double green, double blue);
-
-color texture_value(double u, double v, point3 p);
