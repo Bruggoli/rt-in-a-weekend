4f0f8f205f7f1d0ccadc481409f97016fde0cdbe
markus <marmomag@gmail.com>
2025-11-15 01:35:19 +0100

    forgot to save aabb

--- accel/aabb.c
+++ accel/aabb.c
@@ -37,34 +37,6 @@ aabb aabb_add(aabb box0, aabb box1) {
   return box_out;
 }
 
-bool aabb_hit(aabb* bb ,ray r, interval ray_t) {
-
-
-  point3  ray_orig  = r.orig;
-  vec3    ray_dir   = r.dir;
-
-  for (int axis = 0; axis < 3; axis++) {
-    interval ax = axis_interval(bb, axis);
-    double adinv = 1.0 / ray_dir.e[axis];
-
-    double t0 = (ax.min - ray_orig.e[axis]) * adinv; 
-    double t1 = (ax.max - ray_orig.e[axis]) * adinv;
-
-    if (t0 < t1) {
-      if (t0 > ray_t.min) ray_t.min = t0;
-      if (t1 < ray_t.max) ray_t.max = t1;
-    } else {
-      if (t1 > ray_t.min) ray_t.min = t1;
-      if (t0 < ray_t.max) ray_t.max = t0;
-    }
-
-    if (ray_t.max <= ray_t.min)
-      return false;
-
-  }
-
-  return true;
-}
 
 int longest_axis(aabb* bb) {
   double x = interval_size(bb->x);
